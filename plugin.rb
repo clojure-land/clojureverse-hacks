28aa7ab0f4b393e3ba8acc25dd563d77de4de1e5
Arne Brasseur <arne@arnebrasseur.net>
2017-11-05 15:14:26 +0100

    Start custom styling plugin

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: Clojureverse Hacks
+# about: Custom styling for Clojureverse
+# version: 0.0.1
+# authors: @plexus
+
+register_asset "stylesheets/common/clojureverse.scss"
+register_asset "stylesheets/common/clojureverse_fonts.scss"
